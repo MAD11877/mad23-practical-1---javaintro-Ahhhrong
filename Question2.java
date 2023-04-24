f53dffce5c2743686dadf4036dfd32d80ce962cd
Ahhhrong <S10247585@connect.np.edu.sg>
2023-04-25 00:14:58 +0800

    question 2

--- Question2.java
+++ Question2.java
@@ -9,8 +9,15 @@ public class Question2
      * 
      * Hint: in.nextDouble() to read double
      */
-     
+    System.out.println("What is your Weight?");
+
+    Scanner in = new Scanner(System.in);
+    double weight = in.nextDouble();
+    System.out.println("What is your Height?");
+
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
+    System.out.println(weight/(height*height));
+
   }
 }
